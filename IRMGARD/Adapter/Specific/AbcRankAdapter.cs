6e8d840c116ee4e273247110a8fdf02258956375
Robin-Manuel Thiel <robin-manuel.thiel@thepagedot.de>
2015-11-22 20:35:29 +0100

    Removed unnecessary files

---
+++ IRMGARD/Adapter/Specific/AbcRankAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using IRMGARD.Models;
+using Android.Content;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+
+namespace IRMGARD
+{
+    public class AbcRankAdapter : ArrayAdapter<AbcRankOption>
+    {
+        public AbcRankAdapter(Context context, int resourceId, IList<AbcRankOption> items) : base (context, resourceId, items)
+        {
+        }
+
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = GetItem(position);
+            var view = convertView;
+            if (view == null)
+                view = LayoutInflater.From(Context).Inflate(Resource.Layout.AbcRankOptionItem, null);
+            else
+                ((BitmapDrawable)view.FindViewById<ImageView>(Resource.Id.image).Drawable).Bitmap.Recycle();
+
+
+            if (item.Media != null)
+            {
+                view.FindViewById<TextView>(Resource.Id.letter).Visibility = ViewStates.Gone;
+                var bitmap = AssetHelper.GetBitmap(Context, item.Media.ImagePath);
+                if (bitmap != null)
+                    view.FindViewById<ImageView>(Resource.Id.image).SetImageBitmap(bitmap);
+            }
+            else
+            {
+                view.FindViewById<TextView>(Resource.Id.letter).Text = item.Name;
+            }
+
+            return view;
+        }
+    }
+}
+
