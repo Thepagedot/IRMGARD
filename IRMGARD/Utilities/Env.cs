a67b778c20c435faae03d006f8f6ffdb1bb7ffbd
Frank Wende <devrepo@frankwendeberlin.de>
2016-02-22 20:49:28 +0100

    Add layout files to support devices with screen heights lower than 550dp

--- IRMGARD/Utilities/Env.cs
+++ IRMGARD/Utilities/Env.cs
@@ -7,9 +7,6 @@ namespace IRMGARD
         public const bool Debug = true;
         public const bool Release = !Debug;
 
-        public const string AssetImageDir = "Images";
-        public const string AssetSoundDir = "Sounds";
-
         public static bool LollipopSupport
         {
             get { return Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop; }
