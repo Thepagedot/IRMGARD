b08b42b38ce98e0020714fd17a21b1955d4c4abc
Frank Wende <devrepo@frankwendeberlin.de>
2016-03-22 11:41:17 +0100

    Fix merge issue with LetterWriteFragment

--- IRMGARD/Fragments/LetterWriteFragment.cs
+++ IRMGARD/Fragments/LetterWriteFragment.cs
@@ -23,8 +23,6 @@ namespace IRMGARD
         private LinearLayout llGifItems;
         //private Dictionary<int, bool> playedAllGifs = new Dictionary<int, bool>();
 
-        public LetterWriteFragment(Lesson lesson) : base(lesson) {}
-
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.GifAnimation, container, false);
