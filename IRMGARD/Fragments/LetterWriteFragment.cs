246bcaaa5bc45e543b5d7b276dd22a545928af99
Frank Wende <devrepo@frankwendeberlin.de>
2016-03-23 18:14:44 +0100

    Stop running animations on next iteration or lesson

--- IRMGARD/Fragments/LetterWriteFragment.cs
+++ IRMGARD/Fragments/LetterWriteFragment.cs
@@ -21,7 +21,6 @@ namespace IRMGARD
     public class LetterWriteFragment : LessonFragment<LetterWrite>
     {
         private LinearLayout llGifItems;
-        //private Dictionary<int, bool> playedAllGifs = new Dictionary<int, bool>();
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -46,12 +45,9 @@ namespace IRMGARD
         {
             // Add task items to view and attach Drag and Drop handler
             llGifItems.RemoveAllViews();
-            //playedAllGifs = new Dictionary<int, bool>();
             var adapter = new LetterWriteAdapter(Activity.BaseContext, 0, gifItems);
             for (var i = 0; i < gifItems.Count; i++)
             {
-                //playedAllGifs.Add(i, false);
-
                 // Start animation
                 var view = adapter.GetView(i, null, null);
                 var child = ((view as LinearLayout).GetChildAt(0) as GifImageView);
@@ -71,45 +67,28 @@ namespace IRMGARD
                     gifView.StopAnimation();
                 else
                     gifView.StartAnimation();
+            }
+        }
 
-                //var index = llGifItems.IndexOfChild(sender as LinearLayout);
-                //playedAllGifs[index] = true;
-
-                //if (playedAllGifs.Where(x => x.Value == false).ToList().Count == 0)
-                //    FireUserInteracted(true);
-
-                //if (index == 0)
-                //    index++;
-                //else
-                //    index--;
-
-                //var nextChild = llGifItems.GetChildAt(index);
-                //if (nextChild != null && (nextChild as LinearLayout) != null)
-                //{
-                //    ((nextChild as LinearLayout).GetChildAt(0) as GifImageView).StopAnimation();
-                //}
+        private void StopAllAnimations()
+        {
+            for (var i = 0; i < llGifItems.ChildCount; i++)
+            {
+                ((GifImageView)((LinearLayout)llGifItems.GetChildAt(i)).GetChildAt(0)).StopAnimation();
             }
         }
 
         public override void CheckSolution()
         {
-            //for (int i = 0; i < llGifItems.ChildCount; i++)
-            //{
-            //    var child = llGifItems.GetChildAt(i) as LinearLayout;
-
-            //    if (child == null)
-            //        continue;
-
-            //    var gifView = child.GetChildAt(0) as GifImageView;
-
-            //    if (gifView == null)
-            //        continue;
-
-            //    if (gifView.IsAnimating)
-            //        gifView.StopAnimation();
-            //}
-
+            StopAllAnimations();            
             FinishIteration(true, false);
+        }
+ 
+        public override void OnStop()
+        {
+            base.OnStop();
+
+            StopAllAnimations();
         }
     }
 }
