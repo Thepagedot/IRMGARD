efd53352c423d86f5f25ae027873b86289db6c0f
Frank Wende <devrepo@frankwendeberlin.de>
2017-02-17 17:14:49 +0100

    Set background color to white for disabled levels

--- IRMGARD/Adapter/LevelAdapter.cs
+++ IRMGARD/Adapter/LevelAdapter.cs
@@ -40,9 +40,23 @@ namespace IRMGARD
             text1.Typeface = FontHelper.Get(context, FontHelper.Font.StandardBold);
             text1.SetPadding(text1.PaddingLeft, 38, text1.PaddingRight, 38);
 
-			view.SetBackgroundColor(Android.Graphics.Color.ParseColor (items [position].Color));
+            // Reduce alpha when not available
+            if (!items[position].IsEnabled)
+            {
+                view.SetBackgroundColor(Android.Graphics.Color.WhiteSmoke);
+                view.Alpha = (float)0.5;
+            }
+            else
+            {
+                view.SetBackgroundColor(Android.Graphics.Color.ParseColor(items[position].Color));
+                view.Alpha = (float)1;
+            }
+
+            /*
+            view.SetBackgroundColor(Android.Graphics.Color.ParseColor (items [position].Color));
             if (!items[position].IsEnabled)
                 view.Alpha = (float)0.5;
+            */
 
 			return view;
 		}
